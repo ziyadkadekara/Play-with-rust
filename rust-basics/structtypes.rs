5bbbd35f70eae99dcb933fce4173a53be833272a
ZIYAD K <80669764+ZiyadKadekara@users.noreply.github.com>
2023-01-28 00:12:34 +0530

    Added struct types

---
+++ rust-basics/structtypes.rs
@@ -0,0 +1,24 @@
+// Classic struct with named fields
+struct Student { name: String, level: u8, remote: bool }
+
+// Tuple struct with data types only
+struct Grades(char, char, char, char, f32);
+
+fn main() {
+
+// Instantiate classic struct, specify fields in random order, or in specified order
+
+let user_1 = Student { name: String::from("Constance Sharma"), remote: true, level: 2 };
+let user_2 = Student { name: String::from("Dyson Tan"), level: 5, remote: false };
+
+// Instantiate tuple structs, pass values in same order as types defined
+
+let mark_1 = Grades('A', 'A', 'B', 'A', 3.75);
+let mark_2 = Grades('B', 'A', 'A', 'C', 3.25);
+
+println!("{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}", 
+         user_1.name, user_1.level, user_1.remote, mark_1.0, mark_1.1, mark_1.2, mark_1.3, mark_1.4);
+println!("{}, level {}. Remote: {}. Grades: {}, {}, {}, {}. Average: {}", 
+         user_2.name, user_2.level, user_2.remote, mark_2.0, mark_2.1, mark_2.2, mark_2.3, mark_2.4);
+
+}
